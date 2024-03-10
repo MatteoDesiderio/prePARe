d380602f35d518c494b64b52575f333f0721e68b
MatteoDesiderio <4matteo.desiderio@gmail.com>
2024-03-10 13:19:42 +0000

    Reformat

--- objects/par.py
+++ objects/par.py
@@ -4,17 +4,18 @@ The par file class
 from helpers import _get_names, _fill_in_array_shapes
 from .namelist import Namelist
 
+
 class Par:
     def __init__(self, namelists=[]):
         self.namelists = namelists
-        
+
     def __repr__(self):
-        namelists = ["%s"%n for n in self.namelists]
+        namelists = ["%s" % n for n in self.namelists]
         s = "\n\n".join(namelists)
         return s
-    
+
     def from_file(self, path):
         names = _get_names(path)
         namelists = [Namelist.from_file(path, n) for n in names]
-        
+
         self.namelists = _fill_in_array_shapes(namelists)
